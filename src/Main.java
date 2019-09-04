2af6d9626b54d46cd9c34b9718ffc898babd1a40
KathyOlsen <katherinekolsen@verizon.net>
2019-09-04 14:52:54 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        int mynumber;
+        float myothernumber;
+        mynumber = 2;
+        myothernumber = 1.7938f;
+        System.out.println("mynumber is " + mynumber);
+        System.out.println("myothernumber is " + myothernumber);
+
+
+    }
+}
